0052398ce9b5594d86a54250f0e43c8aba1036d9
Raphyyyyy <rafalex.fox@gmail.com>
2024-08-07 14:43:42 -0300

    primeiro commit

---
+++ Portfólium/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>
+      Hello World
+    </h1>
+  );
+}
+
+const student = {
+  name: 'John',
+  age: 33,
+};
+
+console.log(student.name);
+
+export default App;
