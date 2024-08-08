3aae72da1f8bc5bbcf8cb3550640e2ccfb81d054
Raphyyyyy <rafalex.fox@gmail.com>
2024-08-08 18:51:14 -0300

    cards

--- Portfólium/src/App.tsx
+++ Portfólium/src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.tsx';
+import ProjectCard from './components/ProjectCard.tsx';
+import projeto1 from './assets/projeto1.jpg';
 
 function App() {
   return (
     <>
       <Header />
       <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
-      {' '}
-      <div className="h-96" />
+      <div className="p-8 m-12 grid gap-4 items-center justify-between">
+        <ProjectCard projectName="Plataforma de Streaming de Dados" tags={ ['Javascript', 'Tailwind', 'React'] } image={ projeto1 } />
+        <ProjectCard projectName="Plataforma de Streaming de Dados" tags={ ['Javascript', 'Tailwind', 'React'] } image={ projeto1 } />
+        <ProjectCard projectName="Plataforma de Streaming de Dados" tags={ ['Javascript', 'Tailwind', 'React'] } image={ projeto1 } />
+        <ProjectCard projectName="Plataforma de Streaming de Dados" tags={ ['Javascript', 'Tailwind', 'React'] } image={ projeto1 } />
+      </div>
+
     </>
   );
 }
---
+++ Portfólium/src/components/ProjectCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+type CardsProps = {
+  projectName: string;
+  tags: string[];
+  slug: string;
+  image: string;
+};
+
+function ProjectCard({ projectName, tags, slug, image }: CardsProps) {
+  return (
+    <div
+      className="rounded-2xl shadow-xl overflow-hidden max-w-xs h-100% bg-gray-600"
+      name="conteiner-card"
+    >
+      <img
+        src={ image }
+        className="w-full object-cover"
+        name="thumb-projeto"
+      />
+      <div className="p-4" name="div-tags">
+        <h2 className="text-2xl font-bold text-white flex flex-wrap mb-6">{projectName}</h2>
+        <p className="text-white  flex flex-wrap items-center">
+          {
+        tags.map((tag) => (
+          <span key={ tag } className="text-sm bg-gray-700 p-2 rounded-md m-1">{tag}</span>
+        ))
+        }
+        </p>
+      </div>
+      <div name="botao-card">
+        <a href={ `/${slug}` }>
+          <button
+            className="bg-slate-900 p-4 w-full text-white text-sm rounded-md text-xl font-extrabold
+        scale-110 transition-all duration-300 hover:scale-100 "
+          >
+            Ver mais detalhes →
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
