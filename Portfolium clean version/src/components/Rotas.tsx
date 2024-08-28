3c9070a170ef8a99e183c1e2b2921af3ef3d042c
Raphyyyyy <rafalex.fox@gmail.com>
2024-08-28 15:34:17 -0300

    router mudado e main para hash

--- Portfolium clean version/src/components/Rotas.tsx
+++ Portfolium clean version/src/components/Rotas.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Projeto from '../pages/Projeto';
 import NotFound from '../pages/NotFound';
 
 const Rotas: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/Portfolium" element={ <Home /> } />
-      <Route path="/Portfolium/projeto" element={ <Projeto /> } />
-      <Route path="*" element={ <NotFound /> } />
-    </Routes>
+    <Router>
+      <Routes>
+        {/* As rotas devem começar com '/' após o '#' no HashRouter */}
+        <Route path="/" element={<Home />} />
+        <Route path="/projeto" element={<Projeto />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
