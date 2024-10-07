e6b172581d1531951b76bac147b48063f5d875e0
Raphyyyyy <rafalex.fox@gmail.com>
2024-10-07 15:09:59 -0300

    burger menu animado

--- Portfolium clean version/src/components/BurgerMenu.tsx
+++ Portfolium clean version/src/components/BurgerMenu.tsx
@@ -1,34 +1,62 @@
 // src/components/BurgerMenu.js
-import React, { useState } from 'react';
-import '../styles/BurgerMenu.css';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import "../styles/BurgerMenu.css";
 
 const BurgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className = "menu-pai">
-            <div className="burger-icon" onClick={toggleMenu}>
-                <div className={`line ${isOpen ? 'open' : ''}`}></div>
-                <div className={`line ${isOpen ? 'open' : ''}`}></div>
-                <div className={`line ${isOpen ? 'open' : ''}`}></div>
-            </div>
-            {isOpen && (
-                <div className="menu">
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#profile">Sobre Mim</a></li>
-                        <li><a href="#skill">Habilidades</a></li>
-                        <li><a href="#projects">Projetos</a></li>
-                        <li><a href="#projects">Curriculum</a></li>
-                    </ul>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div className="menu-pai">
+      <motion.div
+        className="burger-icon"
+        onClick={toggleMenu}
+        animate={{ rotate: isOpen ? 180 : -180 }} // Rotação de 180 graus
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <div className={`line ${isOpen ? "open" : ""}`}></div>
+        <div className={`line ${isOpen ? "open" : ""}`}></div>
+        <div className={`line ${isOpen ? "open" : ""}`}></div>
+      </motion.div>
+ 
+      {isOpen && (
+        <motion.div
+          animate={{
+            scale: [1, 1.1, 1],
+            y: [10, 20, 0],
+          }}
+          transition={{
+            duration: 1,
+            ease: "easeInOut",
+          }}
+          className="menu"
+        >
+          <ul>
+            <li className="menu_titulo">Menu</li>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#profile">Sobre Mim</a>
+            </li>
+            <li>
+              <a href="#skill">Habilidades</a>
+            </li>
+            <li>
+              <a href="#projects">Projetos</a>
+            </li>
+            <li>
+              <a href="#projects">Curriculum</a>
+            </li>
+          </ul>
+        </motion.div>
+      )}
+    </div>
+  );
 };
 
 export default BurgerMenu;
