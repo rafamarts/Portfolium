154dae494416e9915c7e68ad6694ada952938310
Raphyyyyy <rafalex.fox@gmail.com>
2024-10-10 11:45:21 -0300

    reestruturando

--- Portfólium/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as Projeto } from './Projeto';
-export { default as NotFound } from './NotFound';
-export { default as AddProjeto } from './AddProjeto';
-export { default as Login } from './Login';
