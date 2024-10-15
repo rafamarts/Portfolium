f6220a6e0d3272f1f8abbdb9d47effb69d003072
Rafael Martins <rafamarts@gmail.com>
2024-10-15 11:07:11 -0300

    try 07

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,12 @@ import { BrowserRouter } from 'react-router-dom'; // Importar BrowserRouter
 import App from './App.tsx'
 import './index.css'
 
+const basename = import.meta.env.BASE_URL;
+console.log(basename);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
 
