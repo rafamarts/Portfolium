4bb58a525d8db2f751e4a84384b0deb4b945b117
Raphyyyyy <rafalex.fox@gmail.com>
2024-12-02 18:10:48 -0300

    correcao de alguns itens em projetos

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -1,7 +1,7 @@
 const ProjectData = [
   {
     id: "portfolio-antigo",
-    title: "PORTFÓLIO 2024",
+    title: "Primeiro Porfólio",
     description: "Meu primeiro portfolio, feito com HTML, CSS e JavaScript.",
     fullDescription: `
       Este projeto marca o início da minha jornada como desenvolvedor, criado com um enfoque especial na combinação da tsuru (garça) e da estética da Ásia antiga. Utilizando JavaScript, HTML e CSS, o portfólio não só exibe minhas habilidades e trabalhos anteriores, mas também oferece uma experiência interativa e dinâmica. O portfólio apresenta:<br><br>
@@ -24,17 +24,30 @@ const ProjectData = [
     linkCode: "https://github.com/Raphyyyyy/Portfolio",
   },
   {
-    id: "portfolio-antigo1",
-    title: "Em Breve",
-    description: "Meu primeiro portfolio, feito com HTML, CSS e JavaScript.",
-    fullDescription: "Meu primeiro portfolio, feito com HTML, CSS e JavaScript.",
-    iconimage: "https://placehold.co/600x400/000000/FFF",
-    image1: "https://placehold.co/600x400",
-    image2: "https://placehold.co/600x400",
-    image3: "https://placehold.co/600x400",
-    image4: "https://placehold.co/600x400",
-    headerImage: "https://placehold.co/600x400",
-    techs: ["HTML", "CSS", "JavaScript"],
+    id: "portfolio-2024",
+    title: "Portfólio 2024",
+    description: "O segundo portfolio, mais moderno feito com React, Vite, Framer motion e use effect",
+    fullDescription: `Seja bem-vindo ao meu segundo portfólio, onde a inovação tecnológica encontra a elegância do design. Desenvolvido com as tecnologias mais modernas, como React, Framer Motion e Vite, este portfólio é uma celebração das possibilidades criativas e técnicas que essas ferramentas oferecem.
+<br><br>
+Inspirado pelo tema dos dentes de leão, este portfólio simboliza a fase após o florescimento, onde é hora de voar e explorar novos horizontes. Com um estilo gráfico clean e animações suaves, cada seção foi cuidadosamente projetada para proporcionar uma experiência visualmente agradável e envolvente.
+<br><br>
+<b>Sobre Mim</b><br>
+Nesta seção, você encontrará um pouco sobre minha trajetória, minhas paixões e o que me motiva. É uma oportunidade de conhecer a pessoa por trás dos projetos e entender minha abordagem e filosofia de trabalho.
+<br><br>
+Habilidades<br>
+Aqui, apresento minhas habilidades com destaque para as tecnologias e ferramentas com as quais tenho maior proficiência. Desde desenvolvimento frontend até animações complexas, veja como posso transformar ideias em realidade através de código eficiente e design inovador.
+<br><br>
+Projetos<br>
+Navegue por meus projetos mais recentes, onde aplico todo o meu conhecimento e criatividade. Cada projeto é uma demonstração prática de minhas capacidades, acompanhado de detalhes sobre o processo de desenvolvimento e os desafios superados.
+<br><br>
+Explore este portfólio e descubra como a combinação de tecnologias modernas e um design cuidadosamente elaborado pode criar algo verdadeiramente especial. Vamos voar juntos e ver até onde os ventos nos levam!`,
+    iconimage: "src/assets/Projetos/PortNovoThumb.gif",
+    image1: "/Portfolium/src/assets/Projetos/PortNovoVideo.gif",
+    image2: "/Portfolium/src/assets/Projetos/PortNovo1.png",
+    image3: "/Portfolium/src/assets/Projetos/PortNovo2.png",
+    image4: "/Portfolium/src/assets/Projetos/PortNovo3.png",
+    headerImage: "/Portfolium/src/assets/Projetos/PortNovoThumb.gif",
+    techs: ["React", "FramerMotion", "UseEffect"],
     linkSite: "https://www.google.com",
     linkCode: "https://www.google.com",
   },
